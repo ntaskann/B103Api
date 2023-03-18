895696b6a790a6dd2f51bd2a444c4a02357bb2af
Nurullah Taşkan <nttaskan@gmail.com>
2023-03-18 22:10:32 +0300

    18.03.2023

--- src/test/java/get_requests/Get08.java
+++ src/test/java/get_requests/Get08.java
@@ -1,7 +1,16 @@
 package get_requests;
 
 import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Get08 extends JsonPlaceHolderBaseUrl {
     /*
@@ -25,7 +34,39 @@ public class Get08 extends JsonPlaceHolderBaseUrl {
      */
 
     @Test
-    public void get08(){
+    public void get08() {
+
+        //set the url
+        spec.pathParams("first", "todos", "second", 2);
+
+        //set the expected data
+        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
+        Map<String, Object> expectedData = obj.expectedDataMethod(1, "quis ut nam facilis et officia qui", false);
+
+        expectedData.put("Via", "1.1 vegur");
+        expectedData.put("Server", "cloudflare");
+        System.out.println("expectedData = " + expectedData);
+
+        //send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //do asssertion
+
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.get("userId"), actualData.get("userId"));
+        assertEquals(expectedData.get("completed"), actualData.get("completed"));
+        assertEquals(expectedData.get("title"), actualData.get("title"));
+
+        // And header "Via" is "1.1 vegur"
+        assertEquals(expectedData.get("Via"), response.getHeader("Via"));
+
+        // And header "Server" is "cloudflare"
+        assertEquals(expectedData.get("Server"), response.getHeader("Server"));
+
 
     }
 }
